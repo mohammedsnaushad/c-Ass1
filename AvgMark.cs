fbd4ae8d57db57fd52a0815a3f1b180fd102dd21
mohammedsnaushad <102455221+mohammedsnaushad@users.noreply.github.com>
2022-05-04 14:09:35 +0530

    Add files via upload

---
+++ AvgMark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class mark
+{
+    public static void Main()
+    {
+        int a, b,c,d,e;
+        Console.WriteLine("Enter the mark of first student:");
+        a =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the mark of second student:");
+        b =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the mark of third student:");
+        c =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the mark of fourth student:");
+        d =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the mark of fifth student:");
+        e=Convert.ToInt32(Console.ReadLine());
+
+        if (a > b && a > c && a > d && a > e)
+            Console.WriteLine("The largest mark is : " + a);
+        else if (b > a && b > c && b > d && b > e)
+            Console.WriteLine("The largest mark is : " + b);
+        else if (c > a && c > b && c > d && c > e)
+            Console.WriteLine("The largest mark is : " + c);
+        else if (d > a && d > b && d > c && d > e)
+            Console.WriteLine("The largest mark is : " + d);
+        else
+            Console.WriteLine("The largest mark is : " + e);
+    }
+}
