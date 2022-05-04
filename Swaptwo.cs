1b04ddc466912f6e1f26e1786546852b22be3c02
mohammedsnaushad <102455221+mohammedsnaushad@users.noreply.github.com>
2022-05-04 14:16:52 +0530

    Add files via upload

---
+++ Swaptwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Swp
+{
+    public static void Swap(ref double x,ref double y)
+    {
+        double temp;
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Enter two numbers to swap");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+        Swap(ref x,ref y);
+        Console.WriteLine("After swapping :{0} , {1}",x,y);
+    }
+
+
+}
+
