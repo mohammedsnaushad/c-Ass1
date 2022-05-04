7110378888084487bfbfc3bc98721e9264243710
mohammedsnaushad <102455221+mohammedsnaushad@users.noreply.github.com>
2022-05-04 14:01:15 +0530

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Crc
+{
+    public static void Arper(ref double r)
+    {
+        double area;
+        double permtr;
+        area = Math.PI * r * r;
+        permtr = 2 * Math.PI * r;
+        Console.WriteLine("Area of the circle :" + area);
+        Console.WriteLine("Perimetre of Circle:" + permtr);
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Enter the radius of the circle");
+        double r= Convert.ToDouble(Console.ReadLine());
+        Arper(ref r);
+    }
+}
